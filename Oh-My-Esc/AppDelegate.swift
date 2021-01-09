aaade6ff2b352725cba7805eaea782a95468dd3d
Aksidion Kreimben <aksidionkreimben@gmail.com>
2021-01-09 15:31:56 +0900

    Implement basic.

---
+++ Oh-My-Esc/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Oh-My-Esc
+//
+//  Created by Aksidion Kreimben on 1/8/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    // MARK: Common resource for managing mask monitor.
+    var monitor: Any?
+    // END
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        
+        print("monitor info before remove it from NSEvent: \(String(describing: self.monitor))")
+        NSEvent.removeMonitor(self.monitor!)
+    }
+}
+
