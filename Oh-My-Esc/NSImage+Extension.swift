b3cda9eabae5cd576c0526d50f8fcf37acdf5340
Aksidion Kreimben <aksidionkreimben@gmail.com>
2021-02-25 17:18:26 +0900

    Add extension from stackoverflow.com

---
+++ Oh-My-Esc/NSImage+Extension.swift
@@ -0,0 +1,32 @@
+//
+//  NSImage+Extension.swift
+//  Oh-My-Esc
+//
+//  Created by Aksidion Kreimben on 2/25/21.
+//
+// Code is from [https://stackoverflow.com/questions/11949250/how-to-resize-nsimage]
+//
+
+import Cocoa
+
+extension NSImage {
+    func resized(to newSize: NSSize) -> NSImage? {
+        if let bitmapRep = NSBitmapImageRep(
+            bitmapDataPlanes: nil, pixelsWide: Int(newSize.width), pixelsHigh: Int(newSize.height),
+            bitsPerSample: 8, samplesPerPixel: 4, hasAlpha: true, isPlanar: false,
+            colorSpaceName: .calibratedRGB, bytesPerRow: 0, bitsPerPixel: 0
+        ) {
+            bitmapRep.size = newSize
+            NSGraphicsContext.saveGraphicsState()
+            NSGraphicsContext.current = NSGraphicsContext(bitmapImageRep: bitmapRep)
+            draw(in: NSRect(x: 0, y: 0, width: newSize.width, height: newSize.height), from: .zero, operation: .copy, fraction: 1.0)
+            NSGraphicsContext.restoreGraphicsState()
+            
+            let resizedImage = NSImage(size: newSize)
+            resizedImage.addRepresentation(bitmapRep)
+            return resizedImage
+        }
+        
+        return nil
+    }
+}
