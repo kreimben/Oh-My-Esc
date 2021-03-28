b17e3cb2f702126495c72bd14f17c0db9bbcb95c
Aksidion Kreimben <aksidionkreimben@gmail.com>
2021-03-28 22:44:21 +0900

    Separate codes.

--- Oh-My-Esc/AppDelegate.swift
+++ Oh-My-Esc/AppDelegate.swift
@@ -23,54 +23,20 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
         if IOHIDCheckAccess(requestType) != kIOHIDAccessTypeGranted { IOHIDRequestAccess(requestType) }
         
-        let monitor = NSEvent.addGlobalMonitorForEvents(matching: .keyDown) { (event) in
-            print("Event: \(String(describing: event))")
-            switch event.keyCode {
-            case 53:
-                print("ESC!!!")
-                OMESoundManager.shared.playSound()
-            default: break
-            }
-        }
-        
-        self.monitor = monitor
-        print("monitor info: \(String(describing: monitor))")
-        
         // MARK: Implement menu bar app.
         if let button = statusItem.button {
 
             button.image = NSImage(named: "AppIcon")!.resized(to: NSSize(width: 18, height: 18))
-            
-            self.setMenu()
 
         } else { fatalError() }
+        
+        self.statusItem.menu = OMEMenuMaker.setMenu()
     }
-    
-    func setMenu() {
-        
-        let m = NSMenu()
-        
-        let status = NSMenuItem(title: "Status: \(OMEAuth.hasAuth() ? "On" : "Off")", action: nil, keyEquivalent: "")
-        status.onStateImage = NSImage(named: NSImage.Name("ome_enable"))
-        status.offStateImage = NSImage(named: NSImage.Name("ome_disable"))
-        status.state = OMEAuth.hasAuth() ? .on : .off
-        
-        m.addItem(status)
-
-        let quit = NSMenuItem(title: "Quit this app", action: #selector(quitThisApp), keyEquivalent: "q")
-
-        m.addItem(quit)
-        
-        statusItem.menu = m
-    }
-    
-    @objc
-    func quitThisApp() { exit(-1) }
     
     func applicationWillTerminate(_ aNotification: Notification) {
         
         print("monitor info before remove it from NSEvent: \(String(describing: self.monitor))")
-        NSEvent.removeMonitor(self.monitor!)
+        OMESoundManager.shared.turnOffSound()
     }
 }
 
